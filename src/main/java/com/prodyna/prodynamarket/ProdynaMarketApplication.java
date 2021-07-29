c19c6da415c8e716dba3fa3bcc0f9856e65b19ef
mostafa <mostafa.aboubakr@prodyna.com>
2021-07-29 13:44:40 +0200

    added modelMapper bean

--- src/main/java/com/prodyna/prodynamarket/ProdynaMarketApplication.java
+++ src/main/java/com/prodyna/prodynamarket/ProdynaMarketApplication.java
@@ -1,8 +1,10 @@
 package com.prodyna.prodynamarket;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +13,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.prodyna.prodynamarket.repositories")
 @ComponentScan(basePackages = {"com.prodyna.prodynamarket.controllers", "com.prodyna.prodynamarket.services"})
 public class ProdynaMarketApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProdynaMarketApplication.class, args);
